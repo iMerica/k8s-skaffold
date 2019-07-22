55ea7063440cc53e35b3bdaaf42c0edb4a88ad30
Greg Horejsi <ghorejsi@qwinix.io>
2019-07-22 12:00:32 -0600

    Gating rebuild

--- build.sh
+++ build.sh
@@ -22,18 +22,20 @@ done
 
 if [[ ($tag_exists -ne 1) ]]; then # TODO: Add rebuild force
     docker build --no-cache --build-arg SKAFFOLD_VERSION=$latest -t ${image}:${latest} .
-fi
 
-# Validate versioning
-build_version=$(docker run -it --rm ${image}:${latest} version | tr -d '\r')
+    # Validate versioning
+    build_version=$(docker run -it --rm ${image}:${latest} version | tr -d '\r')
 
-if [ "${build_version}" == "v${latest}" ]; then
-    echo "Applying latest tag"
-    docker tag ${image}:${latest} ${image}:latest
+    if [ "${build_version}" == "v${latest}" ]; then
+        echo "Applying latest tag"
+        docker tag ${image}:${latest} ${image}:latest
 
-    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+        docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-    echo "Pushing validated image to registry..."
-    docker push ${image}:${latest}
-    docker push ${image}:latest
+        echo "Pushing validated image to registry..."
+        docker push ${image}:${latest}
+        docker push ${image}:latest
+    fi
+else
+    echo "Skipping image creation, version already exists."
 fi
